271a7622aa6b779b710be52d4fe75c77b3a3abf5
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2021-05-19 01:35:22 +0300

    last fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
 import imagesDB from "./gallery-items.js";
 const imagesGridRef = document.querySelector(".js-gallery");
 const modalWithImages = document.querySelector(".js-lightbox");
-const imagesGrid = imagesDB
-  .map(
-    ({ preview, description }) =>
-      `<li class="gallery__item"><img class="gallery__image" src=${preview} alt=${description}/></li>`
-  )
-  .join(" ");
+const fullscreenImg = document.querySelector(".lightbox__image");
 
-imagesGridRef.insertAdjacentHTML("afterbegin", imagesGrid);
+const imagesGrid = ({ preview, description, original }) => {
+  const galleryItems = document.createElement("li");
+
+  const galleryImgItem = document.createElement("img");
+
+  galleryImgItem.src = preview;
+  galleryImgItem.alt = description;
+  galleryImgItem.setAttribute("data-fullscreen", original);
+
+  galleryItems.appendChild(galleryImgItem);
+
+  return galleryItems;
+};
+
+const renderGallery = imagesDB.map(imagesGrid);
+imagesGridRef.append(...renderGallery);
 
 imagesGridRef.addEventListener("click", showModalWithImg);
 
 function showModalWithImg(e) {
-  if (e.target.nodeName === "IMG") {
-    openModal();
+  if (e.target.nodeName !== "IMG") {
+    return;
   }
+
+  modalWithImages.classList.add("is-open");
+  fullscreenImg.src = e.target.dataset.fullscreen;
 }
 
-function openModal() {
-  modalWithImages.classList.add("is-open");
+modalWithImages.addEventListener("click", closeModal);
+function closeModal() {
+  modalWithImages.classList.remove("is-open");
+  fullscreenImg.src = "";
+}
+
+modalWithImages.addEventListener("keydown", closeModalOnEscButton);
+
+function closeModalOnEscButton(e) {
+  console.log(e.key);
 }
